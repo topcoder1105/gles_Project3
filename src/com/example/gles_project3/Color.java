c91769af15d88b9164dd19a277d7739c129bd568
Taeho Lee <tae1560@gmail.com>
2013-06-10 03:50:38 +0900

    sangsang

--- src/com/example/gles_project3/Color.java
+++ src/com/example/gles_project3/Color.java
@@ -30,4 +30,26 @@ public class Color {
 	public static final Color skyblue = new Color(0, 100, 100, 255);
 	public static final Color purple = new Color(100, 0, 100, 255);	
 
+	// 16진수 int를 ColorData 클래스로 변경 
+		static Color intToColor(int i) {
+			int b = (i)&0xFF;
+			int g = (i>>8)&0xFF;
+			int r = (i>>16)&0xFF;
+			int a = (i>>24)&0xFF;
+			
+			return new Color(r,g,b,a);
+		}
+		
+		// ColorData 클래스를 16진수 int로 변경
+		static int colorToInt(Color c) {
+			if (c == null) {
+				c = new Color(255, 255, 255, 255);
+			}
+			int rgb = Math.round(c.a * 255);
+			rgb = (rgb << 8) + Math.round(c.r * 255);
+			rgb = (rgb << 8) + Math.round(c.g * 255);
+			rgb = (rgb << 8) + Math.round(c.b * 255);
+			
+			return rgb;
+		}
 }
